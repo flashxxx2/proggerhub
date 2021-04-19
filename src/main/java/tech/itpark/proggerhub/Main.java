3fb9af660fd3f13c764e2604a30201703bff4f73
amaximov <amaximov@i-novus.ru>
2021-04-19 20:52:00 +0300

    initial commit

--- src/main/java/tech/itpark/proggerhub/Main.java
+++ src/main/java/tech/itpark/proggerhub/Main.java
@@ -1,17 +1,14 @@
 package tech.itpark.proggerhub;
 
-public class Main {
-    // psvm + Tab -> main
-    public static void main(String[] args) {
-        // TODO: MVP -> Minimal Valuable Product
-        // videos
-        // users/owners <-> consumers
-        // Domain Model -> предметная область
+import tech.itpark.proggerhub.di.Container;
+import tech.itpark.proggerhub.repository.UserRepository;
+import tech.itpark.proggerhub.service.UserService;
 
-        // 0. Plain Java (manual)
-        // 1. Dependency Lookup -> Service Registry
-        // 2. Dependency Injection -> Container:
-        // 2.1 Сам делал new
-        // 2.2 Найти нужные зависимости, если они требуются для new
+public class Main {
+    public static void main(String[] args) {
+        Container container = new Container();
+        container.register(UserRepository.class, UserService.class);
+        container.wire();
     }
+
 }
--- src/main/java/tech/itpark/proggerhub/di/Container.java
+++ src/main/java/tech/itpark/proggerhub/di/Container.java
@@ -2,16 +2,10 @@ package tech.itpark.proggerhub.di;
 
 import java.util.*;
 
-// 1. Регистрация
-// 2. Связывание зависимостей
 public class Container {
-    /**
-     * "Определения зависимостей"
-     */
+
     private final Set<Class> definitions = new HashSet<>();
-    /**
-     * Живые зависимости
-     */
+
     private final Map<Class, Object> objects = new HashMap<>();
 
     public void register(Class... clz) {
@@ -19,8 +13,6 @@ public class Container {
     }
 
     public void wire() {
-        // definition -> object (map)
-        // TODO: 1. Reflection API
-        // TODO: 2. Проблема графа
+
     }
 }
